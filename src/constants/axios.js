b318578424c5ba56aad4a73d73ab69eacfcf625a
devzero-deploys <72330074+devzero-deploys@users.noreply.github.com>
2020-10-07 23:00:37 -0300

    Update axios.js

--- src/constants/axios.js
+++ src/constants/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: 'https://whatsapp-devzero-backend.herokuapp.com/'
 });
 
 export default instance;
